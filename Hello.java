a79f1ca1438e00304f3db2fe128e37c49a93a912
Mr. Nichols <mrnichol@garlandisd.net>
2015-08-25 11:05:13 -0500

    Starter code

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/**
+ * Hello world program.
+ * @author Computer Science II/III
+ * @version 0.0
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+	// Need some code here
+    }
+}
